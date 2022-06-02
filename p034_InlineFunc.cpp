a007bd9194208a8e5560374103c234d4a218284b
Mr.Sim <56951180+USER1234657@users.noreply.github.com>
2022-06-02 15:05:21 +0900

    Add files via upload

---
+++ p034_InlineFunc.cpp
@@ -0,0 +1,14 @@
+//p034 인라인 함수
+#include <iostream>
+
+inline int SQUARE(int x)
+{
+    return x*x;
+}
+
+int main()
+{
+    std::cout << SQUARE(5) << std::endl;
+    std::cout << SQUARE(12) << std::endl;
+    return 0;
+}
